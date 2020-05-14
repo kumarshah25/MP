e9f24ee02362db595f70c5916cce0be1b4a3da71
kumarshah25 <kumar.shah@spit.ac.in>
2020-05-14 19:58:33 +0530

    'Done'

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { PhotoService } from './services/photo.service';
+import { Camera } from '@ionic-native/camera/ngx';
+import { IonicStorageModule } from '@ionic/storage';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    Camera,
+    PhotoService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
